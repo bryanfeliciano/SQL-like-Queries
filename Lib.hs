29568b6173dab519ab61b391ee9f6c26f8b8056a
bryan <bryanfeliciano892@gmail.com>
2022-05-09 08:17:20 -0500

    Buisness Logic separated from Main

--- Lib.hs
+++ Lib.hs
@@ -1,3 +1,5 @@
+-- Buisness Logic  --
+
 data Name = Name {
     firstName :: String,
     lastName :: String
@@ -5,4 +7,20 @@ data Name = Name {
 
 instance Show Name where
     show (Name first last) = mconcat [first, " " , last]
+
+data GradeLevel = Freshman | Sophmore | Junior | Senior deriving (Show,Eq,Enum,Ord)
+
+data Student  = Student {
+    studentId :: Int,
+    gradeLevel :: GradeLevel,
+    studentName :: Name
+} deriving Show
+
+students :: [Student]
+students =  [(Student 1 Senior (Name "Audre" "Lorde"))
+            ,(Student 2 Junior (Name "Leslie" "Silko"))
+            ,(Student 3 Freshman (Name "Judith" "Butler"))
+            ,(Student 4 Senior (Name "Guy" "Debord"))
+            ,(Student 5 Sophmore (Name "Jean" "Baudrillard"))
+            ,(Student 6 Junior (Name "Julia" "Kristeva"))] 
 

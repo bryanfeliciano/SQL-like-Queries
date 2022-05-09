ff708aea508ef7b2c2e038c94baa640b1d5b8d93
bryan <bryanfeliciano892@gmail.com>
2022-05-09 08:26:36 -0500

    added two of the three core functions

--- Lib.hs
+++ Lib.hs
@@ -1,3 +1,7 @@
+module Lib where
+
+import Control.Monad
+
 -- Buisness Logic  --
 
 data Name = Name {
@@ -24,3 +28,18 @@ students =  [(Student 1 Senior (Name "Audre" "Lorde"))
             ,(Student 5 Sophmore (Name "Jean" "Baudrillard"))
             ,(Student 6 Junior (Name "Julia" "Kristeva"))] 
 
+-- Select allows you to target a property from your students list for example --
+
+_select :: (a -> b) -> [a] -> [b]
+_select prop vals = do
+    val <- vals
+    return (prop val)
+
+-- Where is a bit more complex (you're using guard imported from 
+-- control monad to filter out parameters tha tdont pass your test) -- 
+
+_where :: (a -> Bool) -> [a] -> [a]
+_where test vals = do
+    val <- vals
+    guard (test val)
+    return val
